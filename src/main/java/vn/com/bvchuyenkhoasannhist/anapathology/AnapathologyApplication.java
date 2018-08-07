b3d4a21a992f99970768f3a26444334e76c05fc7
DESKTOP-QCEEE2B\tphan <tphan.dev@gmail.com>
2018-08-07 22:20:17 +0700

    Init project with Spring boot

---
+++ src/main/java/vn/com/bvchuyenkhoasannhist/anapathology/AnapathologyApplication.java
@@ -0,0 +1,12 @@
+package vn.com.bvchuyenkhoasannhist.anapathology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnapathologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnapathologyApplication.class, args);
+	}
+}
